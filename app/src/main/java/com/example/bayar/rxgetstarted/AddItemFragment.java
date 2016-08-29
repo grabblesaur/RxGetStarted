e16dab0df6b2708c5e14d69b90b8d79bbca40f5c
grabblesaur <ghjcnjnfr219532>
2016-08-29 14:58:09 +0900

    rxbinding in action

--- app/src/main/java/com/example/bayar/rxgetstarted/AddItemFragment.java
+++ app/src/main/java/com/example/bayar/rxgetstarted/AddItemFragment.java
@@ -6,11 +6,13 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+
+import com.jakewharton.rxbinding.view.RxView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class AddItemFragment extends DialogFragment {
 
@@ -26,6 +28,10 @@ public class AddItemFragment extends DialogFragment {
     EditText mName;
     @BindView(R.id.et_importance)
     EditText mImportance;
+    @BindView(R.id.add_item)
+    Button mAddItem;
+    @BindView(R.id.cancel)
+    Button mCancel;
 
     private MainActivity mListener;
 
@@ -40,17 +46,22 @@ public class AddItemFragment extends DialogFragment {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
         mListener = (MainActivity) getActivity();
-    }
 
-    @OnClick(R.id.add_item) void addItem() {
-        String name = mName.getText().toString();
-        String importance = mImportance.getText().toString();
-        Task task = new Task(name, Integer.valueOf(importance));
-        mListener.addItem(task);
-        dismiss();
-    }
+        RxView.clicks(mAddItem)
+                .map(aVoid1 -> {
+                    String name = mName.getText().toString();
+                    String importance = mImportance.getText().toString();
+                    if (importance.isEmpty()) {
+                        importance = "0";
+                    }
+                    return new Task(name, Integer.valueOf(importance));
+                })
+                .subscribe(task -> {
+                    mListener.addItem(task);
+                    dismiss();
+                });
 
-    @OnClick(R.id.cancel) void cancelAction() {
-        dismiss();
+        RxView.clicks(mCancel)
+                .subscribe(aVoid -> dismiss());
     }
 }
--- app/src/main/java/com/example/bayar/rxgetstarted/MainActivity.java
+++ app/src/main/java/com/example/bayar/rxgetstarted/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.bayar.rxgetstarted;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import com.jakewharton.rxbinding.view.RxView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity implements AddItemFragment.addItemFromInterface{
+public class MainActivity extends AppCompatActivity
+        implements AddItemFragment.addItemFromInterface{
 
     private static final String TAG = "ghjcnjnfr";
 
     @BindView(R.id.recyclerView)
     RecyclerView mRecyclerView;
+    @BindView(R.id.fab)
+    FloatingActionButton mFab;
 
     private List<Task> mTaskList = new ArrayList<>();
     private TaskAdapter mAdapter;
@@ -32,12 +37,13 @@ public class MainActivity extends AppCompatActivity implements AddItemFragment.a
         mAdapter = new TaskAdapter(mTaskList);
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
 
-    @OnClick(R.id.fab) void onFabClick() {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        AddItemFragment fragment = AddItemFragment.newInstance();
-        fragment.show(transaction, "add_item_dialog");
+        RxView.clicks(mFab)
+                .subscribe(aVoid -> {
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    AddItemFragment fragment = AddItemFragment.newInstance();
+                    fragment.show(transaction, "add_item_dialog");
+                });
     }
 
     @Override
